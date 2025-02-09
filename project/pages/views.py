cec095c59c6dec86093a719aae3b0b01f75c9be7
Jomaha Alshubily <joash1423@gmail.com>
2025-02-09 20:28:11 +0300

    2

--- project/pages/views.py
+++ project/pages/views.py
@@ -6,3 +6,6 @@ from django.shortcuts import render
 def index(request):
     x={'name': 'Jomanah', 'age': 25}
     return render(request, 'pages/index.html', x)
+
+def index2(request):
+    return render(request, 'pages/index2.html')
